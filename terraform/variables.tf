69dee6c08043e58f7ba5b914e23ff405f7f5f054
satish <pathadesatish0@gmail.com>
2026-06-27 23:13:30 +0530

    write variable block

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "logstream"
+}
