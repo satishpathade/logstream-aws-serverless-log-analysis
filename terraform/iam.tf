d5e50e3e0f592203015e09929fd73e2b5472d8fc
satish <pathadesatish0@gmail.com>
2026-06-27 23:17:00 +0530

    iam role for lambda execution

---
+++ terraform/iam.tf
@@ -0,0 +1,27 @@
+# Lambda execution role
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "logstream_lambda" {
+  name = "logstream-lambda-role-dev"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role = aws_iam_role.logstream_lambda.name
+
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
